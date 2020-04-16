9c7e2df529f0a33a89b7ad8a4435de8ce6197e1e
two_down <mister.franz02@gmail.com>
2020-04-16 18:32:26 +0700

    Добавьте файлы проекта.

---
+++ ExtentionMethod.CMD/IntExtention.cs
@@ -0,0 +1,16 @@
+﻿namespace ExtentionMethod.CMD
+{
+    public static class IntExtention
+    {
+        public static bool IsPolindrom(this int number)
+        {
+            var numb = number.ToString();
+
+            for (int i = 0; i < numb.Length / 2; i++)
+                if (numb[i] != numb[numb.Length - i - 1])
+                    return false;
+
+            return true;
+        }
+    }
+}
---
+++ ExtentionMethod.CMD/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ExtentionMethod.CMD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Добро пожаловать.");
+
+            Console.Write("Введите число: ");
+            var numb = int.Parse(Console.ReadLine());
+
+            var answer = numb + " - ";
+
+            if (!numb.IsPolindrom())
+                answer += "не ";
+
+            Console.WriteLine(answer + "палиндром.");
+        }
+    }
+}
